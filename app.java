e16ed5d0e9dfadc391982efd654b539a3fc31e01
Blü <91242410+BluWhiteBear@users.noreply.github.com>
2022-03-10 09:34:01 -0500

    First attempt at buttons in JFrame

--- app.java
+++ app.java
@@ -1,4 +1,6 @@
+import javax.swing.AbstractButton;
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import java.awt.Dimension;
@@ -12,6 +14,7 @@ public class app
         JFrame frame = new JFrame("Flash Cards | Java Swing");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setPreferredSize(new Dimension(640, 360));
+        frame.setLayout(null);
         //Window icon
         ImageIcon icon = new ImageIcon("images/app_icon.png");
         frame.setIconImage(icon.getImage());
@@ -21,6 +24,27 @@ public class app
         //Window Content
         JLabel label = new JLabel("Body Text :)");
         frame.getContentPane().add(label);
+
+        JButton button1 = new JButton("Button 1 Text");
+        button1.setVerticalTextPosition(AbstractButton.CENTER);
+        button1.setHorizontalTextPosition(AbstractButton.LEADING);
+        Dimension size = button1.getPreferredSize();
+        button1.setBounds(100, 50, size.width, size.height);
+
+        frame.getContentPane().add(button1);
+
+
+
+
+        JButton button2 = new JButton("Button 2 Text");
+        button2.setVerticalTextPosition(AbstractButton.CENTER);
+        button2.setHorizontalTextPosition(AbstractButton.CENTER);
+
+        button2.setBounds(300, 180, size.width, size.height);
+
+        frame.getContentPane().add(button2);
+
+
 
 
 
