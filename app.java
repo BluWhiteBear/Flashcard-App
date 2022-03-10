59f70f6d1ffe6051945c21530bde719b5cb6b995
Blü <91242410+BluWhiteBear@users.noreply.github.com>
2022-03-10 09:16:12 -0500

    Centered window at custom resolution

--- app.java
+++ app.java
@@ -1,20 +1,22 @@
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import java.awt.Dimension;
 
 public class app
 {
     private static void buildGUI()
     {
         //Create window
-        //Window name & close behavior
+        //Window name, resolution & close behavior
         JFrame frame = new JFrame("Flash Cards | Java Swing");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setPreferredSize(new Dimension(640, 360));
         //Window icon
         ImageIcon icon = new ImageIcon("images/app_icon.png");
         frame.setIconImage(icon.getImage());
 
-        
+
 
         //Window Content
         JLabel label = new JLabel("Body Text :)");
@@ -23,8 +25,9 @@ public class app
 
 
 
-        //Display window
+        //Display window in center of screen
         frame.pack();
+        frame.setLocationRelativeTo(null);
         frame.setVisible(true);
     }
 
