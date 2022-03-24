2606a7fba89277b01b2e302af4158a1b5e254135
HurryNelson <56083283+HurryNelson@users.noreply.github.com>
2022-03-24 09:15:38 -0400

    cleaned up

--- fileWriter.java
+++ fileWriter.java
@@ -2,13 +2,13 @@ import java.io.FileWriter;
 // import java.io.IOException;
 import java.io.IOException;
 
-public class fileWriter extends mainMenu {
+public class fileWriter extends app {
     public static void main(String[] args) {
         try (
-            FileWriter myWriter = new FileWriter(deckFileName + ".txt")) {
+            FileWriter myWriter = new FileWriter(deckFileName + ".txt", true)) {
                 // myWriter.write("Just had to add try and catch stuff");
                 // myWriter.write("\n1Just had to add try and catch stuff");
-                myWriter.write("\n2Just had to add try and catch stuff");
+                myWriter.write("\nkasdioash");
                 
             } catch (IOException e) {
                 e.printStackTrace();
