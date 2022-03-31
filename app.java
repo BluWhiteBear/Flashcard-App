1cd6101458683dee89be3e9ffd2de1470d92123e
Blü <bluwhitebear@gmail.com>
2022-03-31 02:24:50 -0400

    Beatified the code to make it looks better :)
    <3

--- app.java
+++ app.java
@@ -1,55 +1,45 @@
 import java.awt.*;
-import java.awt.event.*;
 import javax.swing.*;
-import javax.swing.event.*;
-import java.io.FileWriter;
 import java.io.IOException;
-
-
-
-
-
 
 public class app extends JPanel
 {
-    Color backgroundColor = (new Color(24, 24, 24));
-    Color buttonColor = (new Color(35, 35, 35));
-    Color fontColor = (new Color(255, 255, 255));
+	Color backgroundColor = (new Color(24, 24, 24));
+	Color buttonColor = (new Color(35, 35, 35));
+	Color fontColor = (new Color(255, 255, 255));
 
-    static String decks[];
-    static String selectedDeckFileName;
-    
-    //Creates and displays GUI
-    public static JFrame frame = new JFrame("Flash Cards | Java Swing");
+	static String decks[];
+	static String selectedDeckFileName;
 
-    public static void main (String[] args)
+	//Creates and displays GUI
+	public static JFrame frame = new JFrame("Flash Cards | Java Swing");
+
+	public static void main(String[] args)
     {
-        ImageIcon img = new ImageIcon("images/app_icon.png");
-        frame.setIconImage(img.getImage());
-        frame.setResizable(false);
-        frame.setDefaultCloseOperation (JFrame.EXIT_ON_CLOSE);
-        frame.setVisible(true);
+		ImageIcon img = new ImageIcon("images/app_icon.png");
+		frame.setIconImage(img.getImage());
+		frame.setResizable(false);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
 
-        displayMenu(frame);
-    }
+		displayMenu(frame);
+	}
 
-    static void displayFlashcards (JFrame window) throws IOException
+	static void displayFlashcards(JFrame window) throws IOException
     {
-        window.getContentPane().removeAll();
-        window.getContentPane().add(new flashcard());
-        window.revalidate();
-        window.repaint();
-        frame.pack();
-    }
+		window.getContentPane().removeAll();
+		window.getContentPane().add(new flashcard());
+		window.revalidate();
+		window.repaint();
+		frame.pack();
+	}
 
-    static void displayMenu (JFrame window)
+	static void displayMenu(JFrame window)
     {
-        window.getContentPane().removeAll();
-        window.getContentPane().add(new mainMenu());
-        window.revalidate();
-        window.repaint();
-        frame.pack();
-    }
+		window.getContentPane().removeAll();
+		window.getContentPane().add(new mainMenu());
+		window.revalidate();
+		window.repaint();
+		frame.pack();
+	}
 }
-
-
