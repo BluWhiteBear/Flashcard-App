acd79e39917926900b7bbeccdec4eb5680145ad3
Blü <bluwhitebear@gmail.com>
2022-03-30 20:11:52 -0400

    Updated code styling and removed empty space

--- fileCreator.java
+++ fileCreator.java
@@ -5,25 +5,32 @@ import java.io.IOException;
 
 
 
-public class fileCreator extends app {
 
-    public static void newFile(String deckFileName) {
-        try (
-            FileWriter myWriter = new FileWriter("decks/" + deckFileName + ".txt", true)) {
 
-                myWriter.write(deckFileName + " Deck");
-                
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            System.out.println("Successfully created the file: " + deckFileName + ".txt");
+public class fileCreator extends app
+{
+
+    public static void newFile(String deckFileName)
+    {
+        try (FileWriter myWriter = new FileWriter("decks/" + deckFileName + ".txt", true))
+        {
+            myWriter.write(deckFileName + " Deck"); 
+        } 
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+
+        System.out.println("Successfully created the file: " + deckFileName + ".txt");
     }
 
-    public static void delFile() {
+    public static void delFile()
+    {
         
     }
 
-    public static void editFile() {
+    public static void editFile()
+    {
         
     }
     
